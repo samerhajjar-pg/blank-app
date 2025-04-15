38ce81c341a1936252a50b81dea6f32bf2d7327d
samerhajjar <samer.hajjar@publicisgroupe.com>
2025-04-15 11:45:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.button("Submit",)
+
+st.file_uploader("Load spreadsheet",["xlsx","csv"])
