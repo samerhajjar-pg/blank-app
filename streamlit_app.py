aad7093235a7bee9b3b162ad73fd86ae68388f76
samerhajjar <samer.hajjar@publicisgroupe.com>
2025-04-15 12:27:56 +0000

    secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+client_id=st.secrets["id"]
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
